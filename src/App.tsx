a477a58548545d6cac862baf83b3a27973590f28
Alesei N <github.com@bzik.net>
2023-06-01 13:52:53 -0500

    feat: :lipstick: adding basic styling for app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      Nostrud sint ex cupidatat enim cupidatat duis. Laboris proident qui sit ea irure id enim irure commodo. Et eu labore exercitation ea cupidatat dolor. Duis aute eiusmod duis velit sunt amet velit. Tempor aute est mollit nulla nostrud do ea labore ullamco et ullamco deserunt reprehenderit in.
     </div>
   );
 }
