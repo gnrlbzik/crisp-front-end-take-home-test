faa2c7a027ca8c09d6c224722fd5958340e2be08
Alesei N <github.com@bzik.net>
2023-06-02 16:47:09 -0500

    feat: :sparkles: adding activity indicator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ActivityIndicator from './components/ActivityIndicator';
 import './App.scss';
 
 function App() {
   return (
     <div className="App-container">
-      Nostrud sint ex cupidatat enim cupidatat duis. Laboris proident qui sit ea irure id enim irure commodo. Et eu labore exercitation ea cupidatat dolor. Duis aute eiusmod duis velit sunt amet velit. Tempor aute est mollit nulla nostrud do ea labore ullamco et ullamco deserunt reprehenderit in.
+      <ActivityIndicator labelText='Retrieving Products Data' />
     </div>
   );
 }
---
+++ src/components/ActivityIndicator/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import type { ReactElement } from "react";
+
+import './styles.scss';
+
+
+type ActivityIndicatorProps = {
+  labelText?: string,
+}
+
+export default function ActivityIndicator ({ labelText } : ActivityIndicatorProps): ReactElement {
+  return (
+    <div className="ActivityIndicator-container">
+      <div className="activity-progress-indicator"><div></div><div></div><div></div><div></div></div>
+      <div className="activity-indicator-label">{labelText}</div>
+    </div>
+  );
+}
