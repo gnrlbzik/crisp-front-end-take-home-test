136f75f40df58b77d63330a7c9bb271a35cc3723
Alesei N <github.com@bzik.net>
2023-06-13 10:36:35 -0500

    feat: :necktie: adding hook to load table data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DataTable from './components/DataTable';
 import ActivityIndicator from './components/ActivityIndicator';
+
 import './App.scss';
+import SALES_ORDERS_DATA from './data/sales-orders.json';
+
+import type { ITableData } from './types';
 
 function App() {
+  const [ tableData, setTableData ] = useState<ITableData>([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTableData(SALES_ORDERS_DATA);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App-container">
-      <ActivityIndicator labelText='Retrieving Products Data' />
+      {tableData.length ? (
+        <DataTable />
+        ) : (
+        <ActivityIndicator labelText='Retrieving Products Data' />
+      )}
     </div>
   );
 }
