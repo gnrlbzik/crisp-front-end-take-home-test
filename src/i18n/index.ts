3720484620b0bd699ad0a95334d54d0d0d1fde53
Alesei N <github.com@bzik.net>
2023-06-14 16:26:03 -0500

    feat: :globe_with_meridians: adding fetching data locale string

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,22 +4,24 @@ import type { I18nStringsDefinition, I18nLocale } from '../types';
 const i18nStrings: I18nStringsDefinition = {
   'en_US': {
     'default': 'No i18n string reference was found.',
+    'prepping_data': 'Prepping fetched data.',
     'fetching_data': 'Fetching data.',
   },
   'es_MX': {
     'default': 'No se encontró ninguna referencia de cadena i18n.',
+    'prepping_data': 'Preparando los datos obtenidos.',
     'fetching_data': 'Recuperacion de datos.',
   },
 };
 
-export function getContentString(textId: string = 'default', locale: I18nLocale = 'en_US',) {
+export function getContentString(textId: string = 'default', locale: I18nLocale = 'en_US') {
   const i18nString = i18nStrings[locale][textId || 'default'];
 
   return i18nString;
 }
 
 const defaultExport = {
-  get: getContentString
+  get: getContentString,
 };
 
 export default defaultExport;
